3fedd79579ce1605bfedaa6854169fca41865a11
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2023-09-19 17:02:40 +0200

    refinements

---
+++ data/20230918/do-train.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c462823bce125902b4f972c40928099bcebe9cdffbd4e1a401912599e90e09d
+size 420
--- data/20230918/run-train.sh
+++ data/20230918/run-train.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2442d561752d8e1ca9b2433913c51efac3623a68b17f4531331c85b86c315a89
-size 389
+oid sha256:4ab114170be551edee5ed3297e1dc14f9b35b1c0d1d64b75085a0ccaf3fe6b7e
+size 395
